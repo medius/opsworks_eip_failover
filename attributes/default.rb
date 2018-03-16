29c155528fffa7113b3c41d8afb52523de21f974
Puru Choudhary <puruchoudhary@gmail.com>
2018-03-16 15:49:00 -0700

    Increase the cycles to 3

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default[:eip_failover][:port] = 80
 default[:eip_failover][:protocol] = "HTTP"
 default[:eip_failover][:request_path] = "/"
 default[:eip_failover][:timeout] = 30 # seconds
-default[:eip_failover][:cycles] = 2 # consecutive failures
+default[:eip_failover][:cycles] = 3 # consecutive timeouts before failover is triggered
